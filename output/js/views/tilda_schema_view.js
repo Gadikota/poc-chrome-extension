b00e5ea884de3e3e63367dbc636f6ccf0640a17f
Gadikota Mohan Raja <mohan@devathon.com>
2016-08-15 23:18:31 +0530

    Fixes
    1. In the docs AND in the tool, use double-click to toggle on/off the "related object highlighting" features, and in the tool, remove the Reset button.
    2. _tilda.Patients.graph.json should include both the rendered SVG for the Gen Utility, AND the positions saved.

--- output/js/views/tilda_schema_view.js
+++ output/js/views/tilda_schema_view.js
@@ -1,11 +1,16 @@
 define(['text!../templates/tilda_schema/_new.html', "../core/parser"], function(_NewView, _Parser){
   var Backbone = require('backbone');
   var _ = require('lodash');
+  var promiseError = function(error, reject){
+    console.error(error.message);
+    console.error(error.stack);
+    reject(error)
+  }
   var TildaSchemaView = Backbone.View.extend({
+    schemaName: null,
     events: {
-      'change input[type=file][name="schema-file"]': 'handleFileInput',
+      'click a[name="schema-file"]': 'handleFileInput',
       'click .saveSchema': 'saveSchema',
-      'click .reset': 'resetView',
       'change input:radio[name="showObj"]': "togglePapers"
     },
     togglePapers: function(){
@@ -22,37 +27,106 @@ define(['text!../templates/tilda_schema/_new.html', "../core/parser"], function(
       that.$el.html(_NewView);
       return this;
     },
-    handleFileInput: function(event){
-      var that = this;
-      var file = $(event.target)[0].files[0]; // only one file at a time.
-      if(file == null){
-        return true;
-      }
-      $('input:radio[name="showObj"]:first').prop("checked", true); // hack TODO find better way
-      $("#view_c").html("");
-      $("#obj_c").html("");
-      $("#obj_c").show();
-      $("#view_c").show();
-      chrome.storage.local.get(null, function(items) {
-        var allKeys = Object.keys(items);
-        window.tildaCache = items; // used across 
+    schemaFrom: function(fileName, entry){
+      var p = new Promise(function (resolve, reject) {
         var readFile = function(_file){
           var reader = new FileReader();
           reader.onload = function(event) {
             try{
               var schema = JSON.parse(event.target.result);
-              that.schemaParser_object = new _Parser(_.clone(schema), "obj_c", {viewOnly: false});
-              that.schemaParser_view = new _Parser(_.clone(schema), "view_c", {viewOnly: true});
+              resolve(schema)
             } catch(e){
               console.error("Error occured -> "+e.message);
               console.error(e.stack);
+              reject(e);
             }
           };
           reader.readAsText(_file);
         }
-        readFile(file);
+        entry.getFile(fileName, {create: false}, function(fileEntry){
+          fileEntry.file(function(file){
+            readFile(file);
+          })
+        }, function(error){
+          promiseError(error, reject)
+        })
       })
-
+      return p;
+    },
+    initCache: function(fileName, entry){
+      var p = new Promise(function (resolve, reject) {
+        var readFile = function(_file){
+          var reader = new FileReader();
+          reader.onload = function(event) {
+            try{
+              var tildaCache = JSON.parse(event.target.result).tildaCache || {};
+              resolve(tildaCache)
+            } catch(e){
+              resolve({})
+            }
+          };
+          reader.readAsText(_file);
+        }
+        entry.getFile(fileName, {create: false}, function(fileEntry){
+          fileEntry.file(function(file){
+            readFile(file);
+          })
+        }, function(error){
+          resolve({});
+          // promiseError(error, reject)
+        })
+      })
+      return p;
+    },
+    initPackageInfo: function(entry){
+      var p = new Promise(function (resolve, reject) {
+        var readFile = function(_file){
+          var reader = new FileReader();
+          reader.onload = function(event) {
+            try{
+              var packageInfo = JSON.parse(event.target.result) || {};
+              resolve(packageInfo)
+            } catch(e){
+              reject(e);
+            }
+          };
+          reader.readAsText(_file);
+        }
+        entry.getFile("_tilda.packageInfo.json", {create: false}, function(fileEntry){
+          fileEntry.file(function(file){
+            readFile(file);
+          })
+        }, function(error){
+          promiseError(error, reject)
+        })
+      })
+      return p;
+    },
+    handleFileInput: function(event){
+      var that = this;
+      var error = function(error){
+        console.log(error.message);
+        console.log(error.stack);
+      }
+      var init = function(entry){
+        var p = that.initPackageInfo(entry)
+        p.then(function(pkgInfo){
+          var cacheName = "_tilda."+pkgInfo.schema.name+".graph.json";
+          that.initCache(cacheName, entry).then(function(cache){
+            window.tildaCache = cache;
+            var schemaFname = "_tilda."+pkgInfo.schema.name+".json";
+            that.schemaFrom(schemaFname, entry).then(function(schema){
+              that.schemaParser_object = new _Parser(_.clone(schema), "obj_c", {viewOnly: false});
+              that.schemaParser_view = new _Parser(_.clone(schema), "view_c", {viewOnly: true});
+            }).catch(error);
+          }).catch(error);
+        })
+        p.catch(error);
+      }
+      chrome.fileSystem.chooseEntry({ type: 'openDirectory'},  function(entry, fileEntries) {
+        init(entry);
+      });
+      return 0;
     },
     saveSchema: function(event){
       // TODO write to a file.
